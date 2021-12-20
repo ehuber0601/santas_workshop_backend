597c20a1b652e48cf7ea06716ff903d40cbe705b
Eric Huberman <ehuber0601@gmail.com>
2021-12-20 14:04:57 -0500

    Updated Seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,7 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-user = User.create(username: "test", password: "test")
+# user = User.create(username: "test", password: "test")
+cart= Cart.create(product: Product.find(1), user: User.find(1), qty: 1)
+cart= Cart.create(product: Product.find(2), user: User.find(1), qty: 1)
+cart= Cart.create(product: Product.find(3), user: User.find(1), qty: 1)
